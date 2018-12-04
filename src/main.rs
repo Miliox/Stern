9c522672738003ad0225ca4195fb031f6063f0ef
Emiliano Firmino <emiliano.firmino@gmail.com>
2018-12-04 11:27:15 +0100

    Load room file to memory
    Signed-off-by: Emiliano Firmino <emiliano.firmino@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 mod cpu;
 
 use std::env;
+use std::fs::File;
+use std::io::Read;
 
 fn main() {
-    if let Some(room) = env::args().nth(1) {
-        println!("Room: {:?}", room);
+    if let Some(filename) = env::args().nth(1) {
+        println!("Room: {:?}", filename);
+
+        let mut room = Vec::new();
+        let mut file = File::open(filename).unwrap();
+        file.read_to_end(&mut room).unwrap();
+
+        println!("Size: {:?} bytes", room.len());
     }
 
     let r = cpu::Cpu::new();
