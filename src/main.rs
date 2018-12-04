3b8bda94706630205c4502ef07df0e79caf3583f
Emiliano Firmino <emiliano.firmino@gmail.com>
2018-12-04 11:19:29 +0100

    Pass room filename as argument
    Signed-off-by: Emiliano Firmino <emiliano.firmino@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 mod cpu;
 
+use std::env;
+
 fn main() {
+    if let Some(room) = env::args().nth(1) {
+        println!("Room: {:?}", room);
+    }
+
     let r = cpu::Cpu::new();
     println!("{:?}", r);
 }
